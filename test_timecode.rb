5fc2bbc2a77ed866c41da1ef386388f35d991df2
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2008-08-10 22:31:51 +0000

    Another commit

--- test_timecode.rb
+++ test_timecode.rb
@@ -91,7 +91,12 @@ class TimecodeTest < Test::Unit::TestCase
     tc = Timecode.parse_with_fractional_seconds(fraction, 12.5)
     assert_equal "00:00:07:02", tc.to_s
   end
-
+  
+  def test_parse_with_calculation
+    tc = Timecode.parse_with_calculation("00:00:00:15 +2f")
+    assert_equal Timecode.new(17), tc
+  end
+  
   def test_from_seconds
     fraction = 7.1
     tc = Timecode.from_seconds(fraction, 10)
